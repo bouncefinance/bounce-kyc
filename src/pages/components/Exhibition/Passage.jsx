ada53932c7b6ae02c05a96e685662b224b4ded9c
XuMinhui <yitian_2018@163.com>
2021-02-22 11:16:23 +0800

    repair bug

--- src/pages/components/Exhibition/Passage.jsx
+++ src/pages/components/Exhibition/Passage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { PassageStyled } from './styled'
 
-export default function Passage({ title, desc, width, marginTop }) {
+export default function Passage({ title, desc, width, marginTop, children }) {
     return (
         <PassageStyled width={width} marginTop={marginTop}>
             <p className="title">{title}</p>
             <p className="desc">{desc}</p>
+            {children}
         </PassageStyled>
     )
 }
